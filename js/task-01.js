8798f70be799715e4aefefc653d6340b42a8d516
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-07-17 19:45:14 +0300

    Добавляет решение 002
    Добавляет решение 002

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const numberOfItems = document.querySelectorAll('.item');
 console.log(`В списке  ${numberOfItems.length} категории`);
-const itemsHeaders = numberOfItems.forEach(items => {
+const numberOfItemElements = numberOfItems.forEach(items => {
   console.log(`Категория: ${items.querySelector('h2').textContent}`);
   console.log(`Количество элементов: ${items.querySelectorAll('li').length}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const listElement = ingredients.map(item => {
+  const ingredientsListElement = document.createElement('li');
+  ingredientsListElement.textContent = item;
+  return ingredientsListElement;
+});
+ingredientsList.append(...listElement);
