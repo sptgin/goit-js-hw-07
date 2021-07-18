5c7b8a9971b61a6c598542ad561c89d9ff39999b
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-07-19 01:15:02 +0300

    Добавляет решение 006
    Добавляет решение 006

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,7 @@
-
+const inputValidation = document.querySelector('#validation-input');
+inputValidation.addEventListener('input', event => {
+  inputValidation.textLength === 6
+    ? console.log('+++' + inputValidation.textLength)
+    : console.log(inputValidation.textLength);
+  console.log(inputValidation);
+});
