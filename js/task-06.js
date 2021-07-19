935b1d0c1063b5b4b70d6f125bdd93d89b1104f7
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-07-19 18:28:09 +0300

    Добавляет вариант решения 006
    Добавляет вариант решения 006

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,8 @@
 const inputValidation = document.querySelector('#validation-input');
-inputValidation.addEventListener('focusout', event => {
-  inputValidation.textLength == inputValidation.getAttribute('data-length')
+console.log(inputValidation);
+inputValidation.addEventListener('input', event => {
+  inputValidation.value.length ===
+  Number(inputValidation.getAttribute('data-length'))
     ? (inputValidation.classList.add('valid'),
       inputValidation.classList.remove('invalid'))
     : (inputValidation.classList.add('invalid'),
